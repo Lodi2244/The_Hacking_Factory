5106623786e3f0b3e4229f331b3002280e2e21d3
Lodi2244 <elodieletellier1@gmail.com>
2018-10-25 20:32:06 +0200

    Factory done

--- App/db/seeds.rb
+++ App/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  assembly = Assembly.create!(name: Faker::Name.name)
+end
+
+10.times do
+  y = Part.create!(part_number: Faker::Number.between(1, 10))
+  x = Faker::Number.between(1, 10)
+  y.assemblies << Assembly.find(x)
+end
